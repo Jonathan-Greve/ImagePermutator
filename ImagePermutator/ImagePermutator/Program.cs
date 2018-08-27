9b87adb2dc2f13587a404ff40f9e44bb3d7b5c3f
Jonathan Bjørn Greve <jonagrev@gmail.com>
2018-08-27 17:11:44 +0800

    Made class public and removed unused using statements.

--- ImagePermutator/ImagePermutator/Program.cs
+++ ImagePermutator/ImagePermutator/Program.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Drawing;
-using System.IO;
+﻿using System.Drawing;
 
 namespace ImagePermutator
 {
-    class ImageSheet
+    public class ImageSheet
     {
         private int numRows, numCols;
         private int borderThicknessW, borderThicknessH;
